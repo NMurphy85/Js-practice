dccb384ff6915e2a90532dcc77aaf3e3f94497ea
NMurphy85 <murphy.nathant@gmail.com>
2025-12-28 20:01:33 -0500

    Learning data types and practicing in the console

---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("Hello, World!nnnnn");
+console.log(`hello world`[10]);
+console.log("11111".length);
+let str ="This is a sample string for testing";
+console.log(str[str.length - 1]);
+  let LastName =`murphy`;
+  LastName = LastName + "Sama";
+
+  console.log(LastName);
+
+//   F = C * 1.8 + 32
+
+
+  let celsius = 10;
+  let fahrenheit = celsius * 1.8 + 32;
+
+  console.log(fahrenheit);
+ let boo = false;
+ console.log(boo);
