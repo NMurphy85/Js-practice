c1c7ae13c01c25e82992135e72579d444f964584
NMurphy85 <murphy.nathant@gmail.com>
2025-12-31 23:33:46 -0500

    learning how to use for loops

--- script.js
+++ script.js
@@ -1,15 +1,72 @@
-let cash = 40
-let price = 50
-let difference = cash - price
+// let cash = 40
+// let price = 50
+// let difference = cash - price
 
-if (cash > price) {
-    console.log(`you paid extra - here's ${difference} your change`)
-}
-else if (cash === price) {
-    console.log("you paid the exact amount, have a nice day")
-}
+// if (cash > price) {
+//     console.log(`you paid extra - here's ${difference} your change`)
+// }
+// else if (cash === price) {
+//     console.log("you paid the exact amount, have a nice day")
+// }
 
-else {
-    console.log(`you don't have enough cash - you still owe ${-difference} dollars`)   
+// else {
+//     console.log(`you don't have enough cash - you still owe ${-difference} dollars`)   
 
+// }
+
+// let val = ''
+// if (val){
+//     console.log(!!val)
+// }
+
+
+// else {
+//     console.log(!!val)
+// }
+
+
+// let hot = false
+// hot? console.log('its hot outside') : console.log('its cold outside')
+
+// let subscribed = true
+// let loggedIn = true
+// // let str = subscribed && loggedIn ? console.log('show video') : console.log('hide video')
+
+// let cash = 50
+// let price = 40
+// let IsStoreOpen = false
+// let str = cash>=price || IsStoreOpen ? "give reciept" : "do not give reciept"
+// console.log(str)
+
+
+// let obese = false
+// let healthy = true
+// let str2 = obese>=healthy? 'eat less' : 'good job!!'
+// console.log(str2)
+
+// let count = 1
+//  while(count <= 100){
+//   console.log(count)
+//     count = count + 1
+//  }
+
+// for(let i = 1; i <= 20; i = ++i){
+//     if(i%5 === 0 && i%3 === 0){
+//     console.log(`${i} -> frontend simplified`)
+//     }
+//      else if(i%3 === 0){
+// console.log(`${i} -> frontend`)
+//     }
+//    else if(i%5 === 0){
+// console.log(`${i} -> simplified`)
+//     }
+//      else{
+// console.log(`${i} -> ${i}`)
+//     }
+
+// }
+const str = 'Frontend Simplified'
+
+for(let i = 0; i < str.length; i++){
+    console.log(str[i])
 }
